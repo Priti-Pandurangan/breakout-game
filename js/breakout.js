065d4f0cf7353357d62f96bddb20a8fc8dd1bfbf
Priti <pripra3014@gmail.com>
2016-05-23 15:40:17 +0530

    added bricks

--- js/breakout.js
+++ js/breakout.js
@@ -1,10 +1,10 @@
 var canvas = document.querySelector("canvas")
 var ctx = canvas.getContext("2d")
 
+var ballRadius = 10
+
 var x = canvas.width / 2
 var y = canvas.height - 30
-
-var ballRadius = 10
 
 var dx = 2
 var dy = -2
@@ -12,10 +12,26 @@ var dy = -2
 var paddleHeight = 10
 var paddleWidth = 75
 var paddleX = (canvas.width - paddleWidth) / 2
-var paddleY = (canvas.height - paddleHeight)
 
 var rightPressed = false
 var leftPressed = false
+
+var brickRowCount = 3
+var brickColumnCount = 5
+var brickWidth = 75
+var brickHeight = 20
+var brickPadding = 10
+var brickOffsetTop = 30
+var brickOffsetLeft = 30
+
+var bricks = []
+
+for(c = 0; c < brickColumnCount; c++) {
+    bricks[c] = []
+    for(r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 }
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler)
 document.addEventListener("keyup", keyUpHandler)
@@ -48,26 +64,50 @@ function drawBall() {
 
 function drawPaddle() {
     ctx.beginPath()
-    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight)
+    ctx.rect(paddleX, 
+    canvas.height - paddleHeight, paddleWidth, paddleHeight)
     ctx.fillStyle = "#0095dd"
     ctx.fill()
     ctx.closePath()
 }
 
+function drawBricks() {
+    for(c = 0; c < brickColumnCount; c++) {
+        for(r = 0; r < brickRowCount; r++) {
+            var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+            var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+ 
+            bricks[c][r].x = brickX
+            bricks[c][r].y = brickY
+ 
+            ctx.beginPath()
+            ctx.rect(brickX, brickY, brickWidth, brickHeight)
+            ctx.fillStyle = "#0095DD"
+            ctx.fill()
+            ctx.closePath()
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+    drawBricks()
     drawBall()
     drawPaddle()
+    
 
-    if (x + dx > canvas.width - ballRadius || x + dx< ballRadius) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx
     }
     if (y + dy < ballRadius) {
         dy = -dy
-    } else if (y + dy > canvas.height - ballRadius) {
+    }
+    else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy
+            if (y = y - paddleHeight) {
+                dy = -dy
+            }
         }
         else {
             alert("Game Over")
